9395a3c4598276d95620408387be06e0c52b4bae
Ant-Leh <anthony4427044270@gmail.com>
2021-08-12 16:12:55 +0200

    "recuperation"

---
+++ src/Command/CreateParticipantsFromCsvCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Command;
+
+
+use App\Entity\Participant;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+
+#[AsCommand(
+    name: 'CreateParticipantsFromCsv',
+    description: 'Import des Participants a partir d\'un csv',
+)]
+class CreateParticipantsFromCsvCommand extends Command
+{
+    private EntityManagerInterface $entityManager;
+
+
+    public function __construct($projectDir,EntityManagerInterface $entityManager)
+    {
+
+        parent::__construct();
+        $this->projectDir = $projectDir; //permet d'atteindre les classes de partout
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure(): void
+    {
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //Convertit le fichier csv en tableau iterable
+        $recupTableau=$this->getCvsRowAsArray();
+
+        $repoParticipant = $this->entityManager->getRepository(Participant::class);
+
+        //Boucle sur le tableau
+        foreach ($recupTableau as $recupTableau)
+        {
+
+            $newParticipant = new Participant();
+            $newParticipant->setPseudo($recupTableau["pseudo"]);
+            dd($newParticipant);
+            $this->entityManager->persist($newParticipant);
+            $this->entityManager->flush();
+
+
+        };
+
+
+
+
+        }
+
+        public function getCvsRowAsArray()
+        {
+            //Chemin du fichier
+            $inputFile = $this->projectDir.'/public/data/participant.csv';
+
+            //Convertisseur
+            $decoder = new Serializer([new ObjectNormalizer()],[new CsvEncoder()]);
+            $test=$decoder->decode(file_get_contents($inputFile),'csv');
+            dd($test);
+            //Conversion
+            return $decoder->decode(file_get_contents($inputFile),'csv');
+
+        }
+}
